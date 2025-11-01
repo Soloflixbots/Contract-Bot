93ebced2dca0fc693821c34e0551cad9638003c3
𝘚𝘰𝘭𝘰𝘧𝘭𝘪𝘹 𝘉𝘰𝘵𝘴 🎩 <soloflix.bots@gmail.com>
2025-11-01 10:14:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-# config.py
-
-API_ID = 22884130       # Your Telegram API_ID
-API_HASH = "a69e8b16dac958f1bd31eee360ec53fa"
-BOT_TOKEN = "8489912478:AAGgN13yEVcFhu6oJDsPJ8927q7XF6qB6Cs"
-MONGO_URI = "mongodb+srv://yoyat19687:byRateKzeofLw90e@cluster0.ysszzi9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # MongoDB URI
-ADMINS = [8026801357]  # List of admin user IDs
-START_PIC = "https://i.ibb.co/9B4GfqD/jsorg.jpg"  # URL or local path to start pic
-HELP_PIC = "https://i.ibb.co/9B4GfqD/jsorg.jpg"    # URL or local path to help pic
+# Made By Soloflix 
+API_ID = 0                    # Your Telegram API_ID
+API_HASH = ""                 # Your Telegram API_HASH
+BOT_TOKEN = ""                # Your Bot Token from @BotFather
+MONGO_URI = ""                # MongoDB Connection URI
+ADMINS = []                   # List of admin user IDs
+START_PIC = ""                # URL or local path to start pic
+HELP_PIC = ""                 # URL or local path to help pic
